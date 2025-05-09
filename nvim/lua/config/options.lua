1ee6d604df8f69521cdb9f25aa4c9189e3929523
Ahmed Siddiqui <ahsiddiqui@equinix.com>
2025-05-09 15:23:35 -0700

    [neovim] set tabstops

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -9,5 +9,5 @@ vim.g.mapleader = " "
 vim.opt.splitright = true
 
 -- Set how tabs are displayed visually without changing their underlying character
---vim.opt.tabstop = 4 -- Display tabs as 4 spaces wide instead of 8
---vim.opt.shiftwidth = 4 -- Use 4 spaces for each level of indentation
+vim.opt.tabstop = 4 -- Display tabs as 4 spaces wide instead of 8
+vim.opt.shiftwidth = 4 -- Use 4 spaces for each level of indentation
